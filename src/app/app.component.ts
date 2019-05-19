28b33c936be2282b8fd29351f12f4bca9f5b3bd4
leonardo souza <leonardo44ls@gmail.com>
2019-05-19 15:06:02 -0300

    Create initial project Edit component.html to add a new title Edit component.ts to modify page title Edit css page style according to Angular tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
+  test01 = 100;
+  test02 = 20 * 40;
+  test03 = 'leonardo de lima souza';
+  aa = 'aa';
 }
+
