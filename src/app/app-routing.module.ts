3f161c3a789d032263b7859fa9b39b3212a85929
Leonardo De Lima Souza <leonardo.delima@linx.com.br>
2019-06-17 11:13:07 -0300

    Implementation of the route to test component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent }      from './heroes/heroes.component';
+import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'test', component: TestComponent }
 ];
 
 @NgModule({
