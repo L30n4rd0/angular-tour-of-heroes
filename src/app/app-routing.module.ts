a6d2e4c0e9ba2c0650606bc265c9649e4e18507b
Leonardo De Lima Souza <leonardo.delima@linx.com.br>
2019-06-17 15:48:19 -0300

    Add the default route to the dashboard and clickable link to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { TestComponent }        from './test/test.component';
-import { Test2Component } from './test2/test2.component';
+import { Test2Component }       from './test2/test2.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'test', component: TestComponent },
   { path: 'test2', component: Test2Component }
 ];
