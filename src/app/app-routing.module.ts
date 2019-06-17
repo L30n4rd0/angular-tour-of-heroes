62ac252bf11b45ce737a0b3d697a68e0d8fb2c8f
Leonardo De Lima Souza <leonardo.delima@linx.com.br>
2019-06-17 17:22:18 -0300

    Navigating to hero details  - Navigating from hero list  - Navigating from hero dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HeroesComponent }      from './heroes/heroes.component';
 import { TestComponent }        from './test/test.component';
 import { Test2Component }       from './test2/test2.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'test', component: TestComponent },
   { path: 'test2', component: Test2Component }
 ];
